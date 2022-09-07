5167c1b74d31f9b1e4102f382d955bcce5c22fb3
sandeep bhingle <sandeepbhingle@gmail.com>
2022-09-07 10:20:41 +0530

    datatypes

---
+++ src/DataTypes.java
@@ -0,0 +1,22 @@
+import java.math.BigInteger;
+
+public class DataTypes {
+    public static void main(String[] args) {
+        byte age = 127;
+        System.out.println(age);
+        char gender = 'M';
+        System.out.println(gender);
+        float number = 10.5f;
+        System.out.println(number);
+
+        double  doubleValue = 1000.5d;
+        System.out.println(doubleValue);
+
+        boolean  booleanValue = true;
+        System.out.println(booleanValue);
+
+        char gen = '定';
+        System.out.println(gen);
+
+    }
+}
