bb866d97f335ae8abe264dcedb83ab9b946ca906
sandeep bhingle <sandeepbhingle@gmail.com>
2022-11-16 20:38:50 +0530

    Array Introduction

---
+++ src/array/Demo1.java
@@ -0,0 +1,16 @@
+package array;
+
+public class Demo1 {
+    public static void main(String[] args) {
+        int a = 100;//single value
+
+        // LHS: we use [] as just notation to differentiate between normal variable and array variable.
+        // we don't write anything inside the notation.
+        int[] array ={10,20,30,40,50} ; //multiple values when we know what values we are going to store
+//        int[] array1 = new int[Integer.MAX_VALUE];
+        int[] array2 = {};
+
+
+
+    }
+}
---
+++ src/array/Demo2AccessElements.java
@@ -0,0 +1,21 @@
+package array;
+
+public class Demo2AccessElements {
+    public static void main(String[] args) {
+       int[] array = {10,20,30,40,50};
+        System.out.println(array);//hexadecimal hashcode
+        System.out.println(array.length);//gets the size of an array
+        //accessing the elements with the help of index
+       /* System.out.println(array[0]);//first element
+        System.out.println(array[1]);
+        System.out.println(array[2]);
+        System.out.println(array[3]);
+        System.out.println(array[4]);*/
+        for (int index = 0; index < array.length; index++) {
+            System.out.println(array[index]);
+        }
+
+
+
+    }
+}
