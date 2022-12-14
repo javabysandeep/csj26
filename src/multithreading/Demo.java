0f33169b626a6f2818196d3b2b07f3e2d80146df
sandeep bhingle <sandeepbhingle@gmail.com>
2022-12-14 09:18:44 +0530

    multithreading

---
+++ src/multithreading/Demo.java
@@ -0,0 +1,24 @@
+package multithreading;
+
+public class Demo {
+    public static void main(String[] args) {
+        Runnable runnable = ()-> System.out.println("run");//other two ways
+        Thread thread = new Thread(runnable);
+        thread.run();
+    }
+}
+
+class Thread implements Runnable {
+    private Runnable target;
+
+    Thread(Runnable target) {
+        this.target = target;
+    }
+
+    public void run() {
+        if (target != null) {
+            target.run();
+        }
+    }
+}
+
