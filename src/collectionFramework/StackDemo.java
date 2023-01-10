33e8f9595c65ff6f7ca6a984997fecc5c5539af4
sandeep bhingle <sandeepbhingle@gmail.com>
2023-01-10 09:05:36 +0530

    Stack

---
+++ src/collectionFramework/StackDemo.java
@@ -0,0 +1,20 @@
+package collectionFramework;
+
+import java.util.Stack;
+
+public class StackDemo {
+    public static void main(String[] args) {
+        //assignment: parenthesis validation using stack
+        Stack<Integer> stack = new Stack();
+        stack.push(10);
+        stack.push(20);
+        stack.push(30);
+        System.out.println("On top of stack "+ stack.peek());
+        System.out.println("Search in stack "+stack.search(90));
+        System.out.println("Is stack empty "+stack.empty());
+        System.out.println("remove element from stack "+stack.pop());
+        System.out.println("On top of stack "+ stack.peek());
+
+    }
+
+}
